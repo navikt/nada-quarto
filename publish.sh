d6a86766b484c0782614b6f2dbd7cc6ea9244a0b
vebjorre <vebjorn.rekkebo@nav.no>
2024-05-08 10:41:10 +0200

    fix: change filename

--- publish.sh
+++ publish.sh
@@ -3,6 +3,6 @@ set -e
 
 quarto render index.qmd
 
-curl -X PUT -F file=@index.html \
+curl -X PUT -F index.html=@index.html \
     "https://${ENV}/quarto/update/${QUARTO_ID}" \
     -H "Authorization:Bearer ${NADA_TOKEN}"
